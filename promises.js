2c5554cb10404a65084bd3dbb8ba31e99ba013a2
sayful-hassan <sayf.fashion@gmail.com>
2025-01-03 20:22:24 +0530

    Done with promises

---
+++ promises.js
@@ -0,0 +1,10 @@
+const bakingcake = new Promise((resolve, reject)=>{
+    let bakingcake=true;
+    if (bakingcake){
+        resolve("It successfull maded");
+    }else{
+        reject("fcuk adeeel pidichu");
+    }
+}
+
+);
